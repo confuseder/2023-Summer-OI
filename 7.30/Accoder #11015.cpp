bc4c4e9121f679b2f027f0f844de02f1eebf188c
confuseder <g200803092018@163.com>
2023-07-30 18:32:38 +0800

    7.30 AC [Accdoer #11015]

---
+++ 7.30/Accoder #11015.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double num;
+
+int main() {
+
+    // freopen("Accoder #11015.in", "r", stdin);
+    // freopen("Accoder #11015.out", "w", stdout);
+
+    scanf("%lf", &num);
+
+    printf("%.6lf", pow(num, 1.0 / 3));
+
+    // fclose(stdin);
+    // fclose(stdout);
+
+    // system("pause");
+
+    return 0;
+}
