322feaed8404dfe68962f47c600f6d42e2feda73
confuseder <g200803092018@163.com>
2023-09-06 18:59:24 +0800

    更新代码模板

---
+++ 9.6/Luogu P1902.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    // freopen("Luogu P1902.in", "r", stdin);
+    // freopen("Luogu P1902.out", "w", stdout);
+
+    
+
+    // fclose(stdin);
+    // fclose(stdout);
+
+    // system("pause");
+
+    return 0;
+}
